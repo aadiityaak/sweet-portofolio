b3125a7f0dea0a2c05f3fedd6cdee4769a589aff
aadiityaak <aadiityaak@gmail.com>
2023-10-12 13:57:21 +0700

    sementara

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,14 @@
-jQuery(document).ready(function ($) {
-  // Fungsi untuk memuat iframe secara lazy saat modal ditampilkan
-  $(".lazy-load-modal").on("shown.bs.modal", function () {
-    var modal = $(this);
-    var spinner = modal.find(".spinner-border");
-    var iframe = modal.find(".lazy-iframe");
+/*!
+  * Sweetweb v1.0.2 (https://websweetstudio.com)
+  * Copyright 2013-2023 websweetstudio.com
+  * Licensed under GPL (http://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)
+  */
+(function (factory) {
+	typeof define === 'function' && define.amd ? define(factory) :
+	factory();
+})((function () { 'use strict';
 
-    if (iframe.length > 0) {
-      var src = iframe.data("src");
-      spinner.hide(); // Sembunyikan spinner
-      iframe.attr("src", src).removeClass("lazy-iframe");
-    }
-  });
-  $(".dropdown-toggle").dropdown();
-});
+	jQuery(function () {});
+
+}));
+//# sourceMappingURL=script.js.map
